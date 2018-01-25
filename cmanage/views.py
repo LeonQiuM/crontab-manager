58f2d189da2b313f9a6bddb0434904054417597d
LeonQiuM <qiumeng627@gmail.com>
2018-01-25 16:46:42 +0800

    🦄Enhancement: 登录页面，静态文件添加

--- cmanage/views.py
+++ cmanage/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django import views
+
 
 # Create your views here.
+
+def home(request):
+    """
+
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        pass
+
+    elif request.method == "POST":
+        pass
+
+    else:
+        pass
+
+
+def login(request):
+    """
+
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        return render(request, 'login.html')
+
+    elif request.method == "POST":
+        pass
+
+    else:
+        pass

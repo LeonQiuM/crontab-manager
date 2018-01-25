830aade5d43654ba37911d1ccb599da634e65834
LeonQiuM <qiumeng627@gmail.com>
2018-01-25 11:43:46 +0800

    😝Enhancement:使用rest-framework，新建序列化文件

---
+++ cmanage/serializers.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Leon"
+# Date: 2018/1/25
+
+
+from cmanage import models
+from rest_framework import serializers
+
+
+class HostSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Host
+        fields = ('host_id', 'hostname', 'host_ip', 'groups', 'rules')
+
+
+class HostGroupSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.HostGroup
+        fields = ('group_id', 'group_name')
+
+
+class RuleRecordSerializers(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.RuleRecord
+        fields = ('id', 'minute', 'hour', 'day', 'month', 'week', 'command')

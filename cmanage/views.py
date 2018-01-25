009d9f8ed62aa44f57ee64c7a71539752d2f23f6
LeonQiuM <qiumeng627@gmail.com>
2018-01-25 11:47:01 +0800

    🚌Enhancement:rest-framework 序列化相关

--- cmanage/views.py
+++ cmanage/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from cmanage import models
+from cmanage import serializers
+from rest_framework import permissions
+
+
+class HostViewSet(viewsets.ModelViewSet):
+    queryset = models.Host.objects.all()
+    serializer_class = serializers.HostSerializers
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class HostGroupViewSet(viewsets.ModelViewSet):
+    queryset = models.HostGroup.objects.all()
+    serializer_class = serializers.HostGroupSerializers
+    permission_classes = (permissions.IsAuthenticated,)
+
+
+class RuleRecordViewSet(viewsets.ModelViewSet):
+    queryset = models.RuleRecord.objects.all()
+    serializer_class = serializers.RuleRecordSerializers
+    permission_classes = (permissions.IsAuthenticated,)
